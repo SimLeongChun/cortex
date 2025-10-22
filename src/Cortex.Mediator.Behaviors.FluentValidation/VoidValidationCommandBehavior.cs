ee94bbfe500ee1343a67c18dfbe02aebaecf9e42
Enes Hoxha <eneshoxha@outlook.com>
2025-10-22 13:18:19 +0200

    v2/feature/ #148: Implement ValidationCommandBehavior<TCommand>
    Update package icon and add validation behavior
    
    Replaced `andyX.png` with `cortex.png` as the package icon in the project files, reflecting a branding update. Updated the `<PackageIcon>` property and asset references in `Cortex.Mediator.Behaviors.FluentValidation.csproj`.
    
    Added a new `ValidationCommandBehavior<TCommand>` class in `VoidValidationCommandBehavior.cs`. This class implements `ICommandPipelineBehavior<TCommand>` and introduces command validation using FluentValidation, throwing a `ValidationException` for validation failures.

---
+++ src/Cortex.Mediator.Behaviors.FluentValidation/VoidValidationCommandBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Cortex.Mediator.Commands;
+using FluentValidation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cortex.Mediator.Behaviors
+{
+    /// <summary>
+    /// Pipeline behavior for validation command execution.
+    /// </summary>
+    public sealed class ValidationCommandBehavior<TCommand> : ICommandPipelineBehavior<TCommand>
+        where TCommand : ICommand
+    {
+        private readonly IEnumerable<IValidator<TCommand>> _validators;
+
+
+        public async Task Handle(TCommand command, CommandHandlerDelegate next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TCommand>(command);
+            var failures = _validators
+                .Select(v => v.Validate(context))
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            if (failures.Count() > 0)
+            {
+                var errors = failures
+                    .GroupBy(f => f.PropertyName)
+                    .ToDictionary(
+                        g => g.Key,
+                        g => g.Select(f => f.ErrorMessage).ToArray());
+
+                throw new Exceptions.ValidationException(errors);
+            }
+
+            await next();
+        }
+    }
+}
