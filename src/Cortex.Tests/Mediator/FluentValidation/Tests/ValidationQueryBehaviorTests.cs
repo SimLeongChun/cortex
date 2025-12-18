a41ed861de3f3f7e5e98a0c7e901fcd6b24723a6
devilishgoat <jcoxey@gmail.com>
2025-12-18 16:00:42 +0000

    hookup the FluentValidation validators with behaviours

---
+++ src/Cortex.Tests/Mediator/FluentValidation/Tests/ValidationQueryBehaviorTests.cs
@@ -0,0 +1,69 @@
+﻿using Cortex.Mediator.Behaviors.FluentValidation;
+using Cortex.Mediator.Queries;
+using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+
+namespace Cortex.Tests.Mediator.FluentValidation.Tests
+{
+    public class FakeQuery : IQuery<string>
+    {
+    }
+
+    public class ValidationQueryBehaviorTests
+    {
+        [Fact]
+        public async Task Handle_ShouldNotThrowExceptionsWhenThereAreNoValidationFailures()
+        {
+            // Arrange
+            var expectedResult = "completed";
+            var validator = new Mock<IValidator<FakeQuery>>();
+            validator.Setup(v => v.ValidateAsync(It.IsAny<IValidationContext>(), default)).Returns(() => Task.FromResult(new ValidationResult { Errors = new List<ValidationFailure>()}));
+            var validators = new IValidator<FakeQuery>[]
+            {
+                    validator.Object,
+            };
+
+            var next = new Mock<QueryHandlerDelegate<string>>();
+            next.Setup(n => n.Invoke()).Returns(Task.FromResult(expectedResult));
+            var systemUnderTest = new ValidationQueryBehavior<FakeQuery, string>(validators);
+
+            // Act
+            var result = await systemUnderTest.Handle(new FakeQuery(), next.Object, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldThrowAnExceptionsWhenThereIsAValidationFailure()
+        {
+            // Arrange
+            var expectedResult = "completed";
+            var validator = new Mock<IValidator<FakeQuery>>();
+            validator.Setup(v => v.ValidateAsync(It.IsAny<IValidationContext>(), default))
+                .Returns(() => Task.FromResult(new ValidationResult 
+                {
+                    Errors =
+                    [
+                        new("some-property", "was invalid")
+                    ] 
+                }
+            ));
+
+            var validators = new IValidator<FakeQuery>[]
+            {
+                    validator.Object,
+            };
+
+            var next = new Mock<QueryHandlerDelegate<string>>();
+            next.Setup(n => n.Invoke()).Returns(Task.FromResult(expectedResult));
+            var systemUnderTest = new ValidationQueryBehavior<FakeQuery, string>(validators);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<Cortex.Mediator.Exceptions.ValidationException>(async () => await systemUnderTest.Handle(new FakeQuery(), next.Object, CancellationToken.None));
+        }
+    }
+}
---
+++ src/Cortex.Tests/Mediator/FluentValidation/Tests/VoidValidationCommandBehaviorTests.cs
@@ -0,0 +1,66 @@
+﻿using Cortex.Mediator.Behaviors;
+using Cortex.Mediator.Commands;
+using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+
+namespace Cortex.Tests.Mediator.FluentValidation.Tests
+{
+    public class FakeVoidCommand : ICommand
+    {
+    }
+
+    public class VoidValidationCommandBehaviorTests
+    {
+        [Fact]
+        public async Task Handle_ShouldNotThrowExceptionsWhenThereAreNoValidationFailures()
+        {
+            // Arrange
+            var expectedResult = "completed";
+            var validator = new Mock<IValidator<FakeVoidCommand>>();
+            validator.Setup(v => v.ValidateAsync(It.IsAny<IValidationContext>(), default)).Returns(() => Task.FromResult(new ValidationResult { Errors = new List<ValidationFailure>()}));
+            var validators = new IValidator<FakeVoidCommand>[]
+            {
+                    validator.Object,
+            };
+
+            var next = new Mock<CommandHandlerDelegate>();
+            next.Setup(n => n.Invoke()).Returns(Task.FromResult(expectedResult));
+            var systemUnderTest = new ValidationCommandBehavior<FakeVoidCommand>(validators);
+
+            // Act 
+            // Assert
+            await systemUnderTest.Handle(new FakeVoidCommand(), next.Object, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldThrowAnExceptionsWhenThereIsAValidationFailure()
+        {
+            // Arrange
+            var expectedResult = "completed";
+            var validator = new Mock<IValidator<FakeVoidCommand>>();
+            validator.Setup(v => v.ValidateAsync(It.IsAny<IValidationContext>(), default))
+                .Returns(() => Task.FromResult(new ValidationResult 
+                { 
+                    Errors = new List<ValidationFailure> 
+                    { 
+                        new ValidationFailure("some-property", "was invalid")
+                    } 
+                }
+            ));
+
+            var validators = new IValidator<FakeVoidCommand>[]
+            {
+                    validator.Object,
+            };
+
+            var next = new Mock<CommandHandlerDelegate>();
+            next.Setup(n => n.Invoke()).Returns(Task.FromResult(expectedResult));
+            var systemUnderTest = new ValidationCommandBehavior<FakeVoidCommand>(validators);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<Cortex.Mediator.Exceptions.ValidationException>(async () => await systemUnderTest.Handle(new FakeVoidCommand(), next.Object, CancellationToken.None));
+        }
+    }
+}
